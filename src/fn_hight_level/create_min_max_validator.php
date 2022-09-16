ddf7de8f190f06384b8d9572971a276ff1086985
Artur <artur-rvachev@rambler.ru>
2022-09-16 17:33:33 +0300

    create_min_max_validator

---
+++ src/fn_hight_level/create_min_max_validator.php
@@ -0,0 +1,34 @@
+<?php
+
+//Написать функцию create_min_max_validator(int $min, int $max) которая возвращает функцию принимающую один аргумент,
+// функция проверяет входит ли аргумент в диапазон от $min до $max включительно.
+
+function create_min_max_validator(int $min, int $max)
+{
+    $arr = range($min, $max);
+    return function ($num) use ($arr) {
+        return in_array($num, $arr);
+    };
+}
+
+$validator = create_min_max_validator(1, 200);
+assert($validator(200));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//$validator = create_min_max_validator(2, 5);
+
+//assert($validator(3));
+//assert(!$validator(6));
